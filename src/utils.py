0630a5b2d17e809a0fe3acc288d30cf6035cba1a
tejangupta <tejangupta8@gmail.com>
2023-05-31 09:57:14 +0530

    Model Prediction

--- src/utils.py
+++ src/utils.py
@@ -41,3 +41,12 @@ def evaluate_model(X_train, y_train, X_test, y_test, models):
     except Exception as e:
         logging.error('Exception occurred during model training')
         raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        logging.error('Exception occurred in load_object function utils')
+        raise CustomException(e, sys)
